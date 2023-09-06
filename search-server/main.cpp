c884ca76702c2b27cad1b81fc48a2980ea9d2786
ProCodeAcademy <135703419+ProCodeAcademy@users.noreply.github.com>
2023-09-06 15:28:43 +0300

    main.cpp

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,4 +1,20 @@
 // Решите загадку: Сколько чисел от 1 до 1000 содержат как минимум одну цифру 3?
 // Напишите ответ здесь:
-
+#include <iostream>
+using namespace std;
+bool has3(int x) {
+    while(x) {
+        if(x%10==3)
+            return true;
+        x/=10;
+    }
+    return false;
+}
+int main() {
+    int c=0;
+    for(int i=1; i<=100; i++)
+        if(has3(i))
+            c++;
+    cout<<c;
+}
 // Закомитьте изменения и отправьте их в свой репозиторий.
